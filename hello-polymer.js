2ecc056f1d7482ad03bac0fcbf7345590b00f94e
Andreas Tielitz <tielitz@thr3.de>
2016-11-28 10:08:17 +0100

    as

---
+++ hello-polymer.js
@@ -0,0 +1,39 @@
+'use strict';
+
+import Polymer from 'polymer';
+import ChildNode from 'child-node';
+
+Polymer({
+  is: "hello-polymer",
+
+  sayHello: function () {
+    return 'Hello';
+  },
+  onSelect: function(e, detail) {
+    //
+  },
+  created: function() {
+    //
+  },
+  ready: function () {
+    this.addEventListener('myCustomEvent', this.sayHello);
+  },
+  nameChangedValue: function(newValue, oldValue) {
+    console.log('Variable name changed value');
+  },
+  properties: {
+    name: {
+      type: String,
+      value: "Dummy",
+      observer: 'nameChangedValue'
+    },
+    narrow: {
+      type: Boolean,
+      value: false
+    },
+    obj: {
+      type: Object,
+      value: function () { return {}; }
+    }
+  }
+});
